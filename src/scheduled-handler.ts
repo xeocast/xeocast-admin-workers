b070ac895ce907fbe55f98c0eef5a46ddc767ab1
Fabián Karaben <fabiankaraben@gmail.com>
2025-05-14 14:19:35 -0300

    refactor: convert video service request from GET with query params to POST with JSON body

--- src/scheduled-handler.ts
+++ src/scheduled-handler.ts
@@ -40,7 +40,7 @@ export async function handleScheduled(event: ScheduledEvent, env: Env, ctx: Exec
 		? 'https://dash-cron-worker.xeocast.workers.dev/video-generation-callback'
 		: 'http://localhost:8787/video-generation-callback';
 
-	console.log('Test:', 6);
+	console.log('Test:', 5);
 		
 	const videoServiceTasksResponse = await fetch('http://video-srv.xeocast.com:8765/tasks', {
 		headers: {
@@ -50,16 +50,20 @@ export async function handleScheduled(event: ScheduledEvent, env: Env, ctx: Exec
 	const videoServiceTasksJson = await videoServiceTasksResponse.json();	
 	console.log('Current tasks on video service:', videoServiceTasksResponse.status, videoServiceTasksJson);
 
+
 	try {
-		const url = new URL(videoServiceUrl);
-		url.searchParams.append('callbackUrl', callbackUrl);
-		url.searchParams.append('sourceAudioBucketKey', podcastToProcess.source_audio_bucket_key);
-		url.searchParams.append('sourceBackgroundBucketKey', podcastToProcess.source_background_bucket_key);
-		const response = await fetch(url.toString(), {
-			method: 'GET',
+		const response = await fetch(videoServiceUrl, {
+			method: 'POST',
 			headers: {
 				'X-API-Key': env.VIDEO_SERVICE_API_KEY,
+				'Content-Type': 'application/json',
+				'Accept': '*/*'
 			},
+			body: JSON.stringify({
+				callbackUrl,
+				sourceAudioBucketKey: podcastToProcess.source_audio_bucket_key,
+				sourceBackgroundBucketKey: podcastToProcess.source_background_bucket_key,
+			}),
 		});
 
 		if (!response.ok) {
