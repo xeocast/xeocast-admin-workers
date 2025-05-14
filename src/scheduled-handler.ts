3307c0d51c8217cb3a17f6b2105591e32cbb34e5
Fabián Karaben <fabiankaraben@gmail.com>
2025-05-14 03:33:57 -0300

    chore: add test log and remove content-type header in scheduled handler

--- src/scheduled-handler.ts
+++ src/scheduled-handler.ts
@@ -40,11 +40,12 @@ export async function handleScheduled(event: ScheduledEvent, env: Env, ctx: Exec
 		? 'https://dash-cron-worker.xeocast.workers.dev/video-generation-callback'
 		: 'http://localhost:8787/video-generation-callback';
 
+	console.log('Test:', 1);
+
 	try {
 		const response = await fetch(videoServiceUrl, {
 			method: 'POST',
 			headers: {
-				'Content-Type': 'application/json',
 				'X-API-Key': env.VIDEO_SERVICE_API_KEY,
 			},
 			body: JSON.stringify({
